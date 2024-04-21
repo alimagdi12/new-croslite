09e5881dcb04f3d9975174b3886cc8c819d249e6
alimagdi12 <alimagdi12367@yahoo.com>
2024-04-21 10:39:17 +0200

    second commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,14 +8,6 @@ require("dotenv").config();
 
 const User = require("../models/user");
 
-const transporter = nodemailer.createTransport(
-  sendgridTransport({
-    auth: {
-      api_key: process.env.SENDGRID_API,
-    },
-  })
-);
-
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
   if (message.length > 0) {
